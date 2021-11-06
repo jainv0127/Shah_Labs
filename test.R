e081e4f2081dc32bae446c733c177c205acf7eb1
jainv0127 <87710681+jainv0127@users.noreply.github.com>
2021-11-06 12:41:55 -0400

    Update test.R

--- test.R
+++ test.R
@@ -1,9 +1,15 @@
+#git add
+#git commit
+#git push
+
 library(tidyverse)
 library(ggplot2)
 library(stringr)
 #input <- read.csv(file = 'ecoli_tRNA_2010', header = TRUE, stringsAsFactors = TRUE)
 input <- read_tsv(file = "ecoli_tRNA_2010.tsv")#, header = F, sep = ',', stringsAsFactors = F)
 key <- read_tsv(file = 'shah_gilchrist_2010_ecoli.tsv')#, header = T, sep = ' ', stringsAsFactors = F)
+rownames(input) <- input$Codon
+removeAnticodon("AAG")
 
 #print(input)
 #ggplot(data = key, aes(x = as.integer(key[,6]), y = as.integer(key[,8])) +
@@ -11,25 +17,68 @@ key <- read_tsv(file = 'shah_gilchrist_2010_ecoli.tsv')#, header = T, sep = ' ',
 #print(input[x,])
 for(val in c(1:nrow(input)
 )){
-  print(val)
-
-  }
-wobble <- function(codon){
+  codon <- input[val,1]
+  print(codon)
+  print(wobble(codon$Codon))
+  #print(wobble(anticodonGenerator(codon)))
   
-  i <- substring(codon,3,3)
-  if(i == "A"){
-    substring(codon,3,3) <- "U"
-  }
-  if(i == "G"){
-    substring(codon,3,3) <- "U"
-  }
-  if(i == "C"){
-    substring(codon,3,3) <- "U"
-  }
-  if(i == "A"){
-    substring(codon,3,3) <- "U"
-  }
 }
+
+makeLists <- function(codon){
+  
+  cognates <- list()
+  ncognates <- list()
+  pcognates <- list()
+  
+  
+}
+
+wobble("AAA")
+
+wobble <- function(anticodon){  
+  
+  makeLists(codon)
+  
+  val <- 1
+
+  print(anticodon)
+    if(substring(anticodon,val,val) == "A"){
+      unlist(strsplit(anticodon,""))
+      anticodon.split <- unlist(strsplit(anticodon,""))
+      wobble <- c("U","C","G", "A")
+      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+      cognates <- c(a)
+     # print(cognates)
+    }
+    if(substring(anticodon,val,val) == "G"){
+      
+      unlist(strsplit(anticodon,""))
+      anticodon.split <- unlist(strsplit(anticodon,""))
+      wobble <- c("U","C")
+      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+      cognates <- c(a)
+     # print(cognates)
+      
+      } 
+    
+    if(substring(anticodon,val,val) == "U"){
+      unlist(strsplit(anticodon,""))
+      anticodon.split <- unlist(strsplit(anticodon,""))
+      wobble <- c("U","C","G", "A")
+      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+      cognates <- c(a)
+     #print(cognates)
+      
+      }
+  return(a)
+}
+
+#wobble rules function
+  #parameter nucleotide
+  #call wobble wobble function
+  #paste specific spot
+
+
 anticodonGenerator <- function(codon){
 
   string_split = strsplit(codon, split = "")
@@ -40,33 +89,184 @@ anticodonGenerator <- function(codon){
   for(val in c(1:3)){
   if (reversed_chars[val] == "A"){
     reversed_chars[val] <- 'T'
+    s<- paste(reversed_chars)
   }
   else if (reversed_chars[val] == 'T'){
     reversed_chars[val] <- "A"
+    s<- paste(reversed_chars)
   }
   else if (reversed_chars[val] == "G"){
     reversed_chars[val] <- "C"
+    s<- paste(reversed_chars)
   }
   else if (reversed_chars[val] == "C"){
     reversed_chars[val] <- "G"
+    s<- paste(reversed_chars)
   }
   }
-  return(reversed_chars)
+  return(s)
 }
-  #codon <- input[val,1]
-  #print(codon)
-  #print(nrow(key))
-  #for(val in c(1:nrow(key))){
+  
 
-    #k <- key[val,2]  
-    #print(k)
-    #if(k == codon){
-      #print(key[val,])
+removeAnticodon <- function(codon){
+  
+  if(input[codon, tRNA]==0){
+    print("nope")
+  }
+  else{
+    print("valid")
+  }
+}
 
-    #}
-  #}
+check.same.codons<- function(set.1,set.2)
+{
+  set.1 <- unlist(strsplit(set.1,","))
+  set.2 <- unlist(strsplit(set.2,","))
+  if (length(set.1) != length(set.2))
+  {
+    return(F)
+  } else {
+    disagree <- setdiff(set.1,set.2)
+    if (length(disagree) >0)
+    {
+      return(F)
+    }
+    disagree <- setdiff(set.2,set.1)
+    if (length(disagree) >0)
+    {
+      return(F)
+    }
+  }
+  return(T)
+}
+
+PNCognate <- function(codon){
+
+  a <- strsplit(codon,",")
+  neighbor <- list()
+  for(val in c(1,3)){
+    if (a[[val]] == "A"){
+      a[[val]] <- "G"
+      neighbor.append(a)
+      a[[val]] <- "C"
+      neighbor.append(a)
+      a[[val]] <- "T"
+      neighbor.append(a)
+    }
+    else if (a[[val]] == "G"){
+      a[[val]] <- "A"
+      neighbor.append(a)
+      a[[val]] <- "C"
+      neighbor.append(a)
+      a[[val]] <- "T"
+      neighbor.append(a)
+    }
+    else if (a[[val]] == "C"){
+      a[[val]] <- "A"
+      neighbor.append(a)
+      a[[val]] <- "G"
+      neighbor.append(a)
+      a[[val]] <- "T"
+      neighbor.append(a)
+    }
+    
+  else if (a[[val]] == "T"){
+    a[[val]] <- "A"
+    neighbor.append(a)
+    a[[val]] <- "G"
+    neighbor.append(a)
+    a[[val]] <- "C"
+    neighbor.append(a)
+  }
+    for(val in c(0,lenght(a))){
+      if (key[val, 2] != 0){
+        if(a[[val,3]] == a[[codon,3]]){
+          if(wobble(codon) != val){
+            
+            pcognate.append(val)
+          }
+          else{
+            
+            ncognate.append(val)
+          }
+        }
+      
+      
+      
+      }
+    }
+  
+  
+  }
+}
 
 
-#use join function(tidyverse)
-  #give key, codon
-#
+
+checkWobble <- function(codon,trna){
+  
+  reverse <- reverse.complement(codon)
+  trna.split <- unlist(strsplit(trna,""))
+  codon.split <- unlist(strsplit(codon,""))
+  reverse.split <- unlist(strsplit(reverse,""))
+  if(reverse.complement(codon) == trna) 
+  {
+    return(1)
+  } else{
+    index <- which(reverse.split != trna.split)
+    if(index == 1)
+    {
+      t.index <- 1
+      c.index <- 3
+    } else if (index == 2){
+      t.index <- 2
+      c.index <- 2
+    } else if (index == 3){
+      t.index <- 3
+      c.index <- 1
+    } else{
+      print("Error")
+    }
+    if (trna.split[t.index] == "C" && codon.split[c.index] == "T")
+    {
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "T" && codon.split[c.index] == "C"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "G" && codon.split[c.index] == "A"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "A" && codon.split[c.index] == "G"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "G" && codon.split[c.index] == "T"){
+      return(non.canonical.wobble)
+    } else if (trna.split[t.index] == "T" && codon.split[c.index] == "G"){
+      return(non.canonical.wobble)
+    } else if (trna.split[t.index] == "A" && codon.split[c.index] == "C"){
+      return(non.canonical.wobble)
+    } else if (trna.split[t.index] == "C" && codon.split[c.index] == "A"){
+      return(non.canonical.wobble)
+    } else if (trna.split[t.index] == "A" && codon.split[c.index] == "A"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "C" && codon.split[c.index] == "C"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "G" && codon.split[c.index] == "G"){
+      return(canonical.wobble)
+    } else if (trna.split[t.index] == "T" && codon.split[c.index] == "T"){
+      return(canonical.wobble)
+    }
+    else{
+      print("Nope")
+    }
+  }
+}
+
+
+EquationThree <- function(anticodon){
+  w <- 0
+  if (lenght(cognates) > 1){
+    
+  }
+  input[anticodon,2] * (.652)
+  cognates
+  
+}
+
+
