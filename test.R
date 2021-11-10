3ccc2d5e80c150fec2b131a99be37aa4a4fe3f95
Alexander Cope <alexandercope@Alexanders-Air.attlocal.net>
2021-11-10 16:36:30 -0500

    Fixing code and adding comments for Vatsal

--- test.R
+++ test.R
@@ -5,112 +5,122 @@
 library(tidyverse)
 library(ggplot2)
 library(stringr)
-#input <- read.csv(file = 'ecoli_tRNA_2010', header = TRUE, stringsAsFactors = TRUE)
-input <- read_tsv(file = "ecoli_tRNA_2010.tsv")#, header = F, sep = ',', stringsAsFactors = F)
-key <- read_tsv(file = 'shah_gilchrist_2010_ecoli.tsv')#, header = T, sep = ' ', stringsAsFactors = F)
-rownames(input) <- input$Codon
-removeAnticodon("AAG")
 
-#print(input)
-#ggplot(data = key, aes(x = as.integer(key[,6]), y = as.integer(key[,8])) +
-  #geom_point()
-#print(input[x,])
-for(val in c(1:nrow(input)
-)){
-  codon <- input[val,1]
-  print(codon)
-  print(wobble(codon$Codon))
-  #print(wobble(anticodonGenerator(codon)))
+complementRules <- function(nucleotide)
+{
+  if (nucleotide == "A")
+  {
+    return("T")
+  } else if (nucleotide == "C"){
+    return("G")
+  } else if(nucleotide == "G"){
+    return("C")
+  } else if (nucleotide == "T"){
+    return("A")
+  }
+}
+
+## Here is a function you can use to get the reverse complement of a codon 
+reverseComplement <- function(seq)
+{
+  seq.split <- unlist(strsplit(seq,split=""))
+  reverse <- rev(seq.split)
+  trna <- unlist(lapply(reverse,complementRules))
+  trna <- paste(trna,collapse='')
+  return(trna)
   
 }
 
-makeLists <- function(codon){
-  
-  cognates <- list()
-  ncognates <- list()
-  pcognates <- list()
-  
-  
-}
 
-wobble("AAA")
-
+## I would work with T rather than U. I think most of our files use T.
 wobble <- function(anticodon){  
   
   makeLists(codon)
   
   val <- 1
-
-  print(anticodon)
-    if(substring(anticodon,val,val) == "A"){
-      unlist(strsplit(anticodon,""))
-      anticodon.split <- unlist(strsplit(anticodon,""))
-      wobble <- c("U","C","G", "A")
-      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
-      cognates <- c(a)
-     # print(cognates)
-    }
-    if(substring(anticodon,val,val) == "G"){
-      
-      unlist(strsplit(anticodon,""))
-      anticodon.split <- unlist(strsplit(anticodon,""))
-      wobble <- c("U","C")
-      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
-      cognates <- c(a)
-     # print(cognates)
-      
-      } 
+  
+  if(substring(anticodon,val,val) == "A"){
+    unlist(strsplit(anticodon,""))
+    anticodon.split <- unlist(strsplit(anticodon,""))
+    wobble <- c("T","C","G", "A")
+    a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+    cognates <- c(a)
+    # print(cognates)
+  }
+  if(substring(anticodon,val,val) == "G"){
     
-    if(substring(anticodon,val,val) == "U"){
-      unlist(strsplit(anticodon,""))
-      anticodon.split <- unlist(strsplit(anticodon,""))
-      wobble <- c("U","C","G", "A")
-      a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
-      cognates <- c(a)
-     #print(cognates)
-      
-      }
+    unlist(strsplit(anticodon,""))
+    anticodon.split <- unlist(strsplit(anticodon,""))
+    wobble <- c("G","A") ## when you do the reverse complement, will give correct wobble rules between tRNA and codon
+    a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+    cognates <- c(a)
+    # print(cognates)
+    
+  } 
+  
+  if(substring(anticodon,val,val) == "T"){
+    unlist(strsplit(anticodon,""))
+    anticodon.split <- unlist(strsplit(anticodon,""))
+    wobble <- c("T","C","G", "A")
+    a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+    cognates <- c(a)
+    #print(cognates)
+    
+  }
+  
+  if(substring(anticodon,val,val) == "C"){
+    unlist(strsplit(anticodon,""))
+    anticodon.split <- unlist(strsplit(anticodon,""))
+    wobble <- c("C")
+    a <- paste0(wobble, anticodon.split[2], anticodon.split[3])
+    cognates <- c(a)
+    #print(cognates)
+    
+  }
+  
   return(a)
 }
 
 #wobble rules function
-  #parameter nucleotide
-  #call wobble wobble function
-  #paste specific spot
+#parameter nucleotide
+#call wobble wobble function
+#paste specific spot
 
 
 anticodonGenerator <- function(codon){
-
+  
   string_split = strsplit(codon, split = "")
   rev_order = nchar(codon):1
   reversed_chars = string_split[[1]][rev_order]
   paste(reversed_chars, collapse = "")
   #print(reversed_chars)
   for(val in c(1:3)){
-  if (reversed_chars[val] == "A"){
-    reversed_chars[val] <- 'T'
-    s<- paste(reversed_chars)
-  }
-  else if (reversed_chars[val] == 'T'){
-    reversed_chars[val] <- "A"
-    s<- paste(reversed_chars)
-  }
-  else if (reversed_chars[val] == "G"){
-    reversed_chars[val] <- "C"
-    s<- paste(reversed_chars)
-  }
-  else if (reversed_chars[val] == "C"){
-    reversed_chars[val] <- "G"
-    s<- paste(reversed_chars)
-  }
+    if (reversed_chars[val] == "A"){
+      reversed_chars[val] <- 'T'
+      s<- paste(reversed_chars)
+    }
+    else if (reversed_chars[val] == 'T'){
+      reversed_chars[val] <- "A"
+      s<- paste(reversed_chars)
+    }
+    else if (reversed_chars[val] == "G"){
+      reversed_chars[val] <- "C"
+      s<- paste(reversed_chars)
+    }
+    else if (reversed_chars[val] == "C"){
+      reversed_chars[val] <- "G"
+      s<- paste(reversed_chars)
+    }
   }
   return(s)
 }
-  
 
-removeAnticodon <- function(codon){
-  
-  if(input[codon, tRNA]==0){
+## Always make sure to pass in inputs as function arguments. Don't rely on global variables.
+## Can get you in trouble. 
+removeAnticodon <- function(input,codon)
+{
+  tgcn <- input %>% filter(Codon == codon) %>% dplyr::select(tRNA) %>% deframe()
+  if(tgcn==0){
     print("nope")
   }
   else{
@@ -141,7 +151,7 @@ check.same.codons<- function(set.1,set.2)
 }
 
 PNCognate <- function(codon){
-
+  
   a <- strsplit(codon,",")
   neighbor <- list()
   for(val in c(1,3)){
@@ -170,14 +180,14 @@ PNCognate <- function(codon){
       neighbor.append(a)
     }
     
-  else if (a[[val]] == "T"){
-    a[[val]] <- "A"
-    neighbor.append(a)
-    a[[val]] <- "G"
-    neighbor.append(a)
-    a[[val]] <- "C"
-    neighbor.append(a)
-  }
+    else if (a[[val]] == "T"){
+      a[[val]] <- "A"
+      neighbor.append(a)
+      a[[val]] <- "G"
+      neighbor.append(a)
+      a[[val]] <- "C"
+      neighbor.append(a)
+    }
     for(val in c(0,lenght(a))){
       if (key[val, 2] != 0){
         if(a[[val,3]] == a[[codon,3]]){
@@ -190,13 +200,13 @@ PNCognate <- function(codon){
             ncognate.append(val)
           }
         }
-      
-      
-      
+        
+        
+        
       }
     }
-  
-  
+    
+    
   }
 }
 
@@ -268,5 +278,43 @@ EquationThree <- function(anticodon){
   cognates
   
 }
+
+makeLists <- function(codon){
+  
+  cognates <- list()
+  ncognates <- list()
+  pcognates <- list()
+  
+  
+}
+
+
+#input <- read.csv(file = 'ecoli_tRNA_2010', header = TRUE, stringsAsFactors = TRUE)
+input <- read_tsv(file = "ecoli_tRNA_2010.tsv")#, header = F, sep = ',', stringsAsFactors = F)
+key <- read_tsv(file = 'shah_gilchrist_2010_ecoli.tsv')#, header = T, sep = ' ', stringsAsFactors = F)
+## the rownames() function with tibbles (dataframes read in from read_tsv) is deprectated, so let's remove that for now.
+
+removeAnticodon(input,"AAG")
+
+#print(input)
+#ggplot(data = key, aes(x = as.integer(key[,6]), y = as.integer(key[,8])) +
+  #geom_point()
+#print(input[x,])
+for(val in 1:nrow(input))
+  {
+  codon <- input[val,1]
+  print(paste("Current codon:", codon$Codon))
+  ## remember to reverse the coding sequence to get anticodon first
+  anticodon <- reverseComplement(codon$Codon)
+  print(paste("Current anticodon:",anticodon))
+  wobbles <- unlist(lapply(wobble(anticodon),reverseComplement))
+  #print(wobble(anticodonGenerator(codon)))
+  print(paste0("Can wobble with codons:",paste(wobbles,collapse=",")))
+}
+
+
+
+wobble("AAA")
+
 
 
